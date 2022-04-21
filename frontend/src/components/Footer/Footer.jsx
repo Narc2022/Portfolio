c2d7f8f0760a3f3fb1d2574ff9677141d5dd6e36
Narc2022 <102596511+Narc2022@users.noreply.github.com>
2022-04-21 11:24:05 +0530

    Update footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -5,13 +5,16 @@ import React from 'react';
 import "./Footer.css";
 const Footer = () => {
 	return (
-		<div className="footer">
+	<div className="footer">
 		<div>
 			<Typography variant="h5">About Me</Typography>
-            <Typography>Hey, my name is Sachin Singh.I am a Full-Stack Developer and a tutorial on Youtube channel called <b>naRc Tech</b></Typography>
+            <Typography>Hey, my name is Sachin Singh.
+				I am a Full-Stack Developer and a tutorial on Youtube channel called 
+				<b>naRc Tech</b>
+			</Typography>
 
 			<Link to="/contact" className="footerContactBtn">
-			<Typography>Contact Us</Typography>
+			<Typography >Contact Us</Typography>
 			</Link>
 		</div>
 		<div>
