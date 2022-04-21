70ae46a9e223e896b436319d9315315205f8e500
Narc2022 <102596511+Narc2022@users.noreply.github.com>
2022-04-21 08:46:34 +0530

    footer icons

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,5 +1,8 @@
-import React from 'react'
-import "./Footer.css"
+import { Link } from 'react-router-dom';
+import { Typography } from '@mui/material';
+import { BsGithub, BsYoutube, BsInstagram, BsLinkedin } from 'react-icons/bs'
+import React from 'react';
+import "./Footer.css";
 const Footer = () => {
 	return (
 		<div className="footer">
@@ -13,8 +16,23 @@ const Footer = () => {
 			<Typography>Contact Us</Typography>
 			</Link>
 		</div>
+		<div>
+			<Typography variant="h6">Social Media</Typography>
+			<a href='https://github.com/Narc2022' target='blank'>
+				<BsGithub />
+			</a>
+			<a href='https://www.youtube.com/channel/UClXZ6LP5dubwI1We2fVkE7g' target='blank'>
+				<BsYoutube />
+			</a>
+			<a href='https://instagram.com/lets_get_programming_update?igshid=YmMyMTA2M2Y=' target='blank'>
+				<BsInstagram />
+			</a>
+			<a href='https://www.linkedin.com/in/sachin-singh-086aa2167' target='blank'>
+				<BsLinkedin />
+			</a>
 		</div>
-	)
+		</div>
+	);
 }
 
-export default Footer
+export default Footer;
