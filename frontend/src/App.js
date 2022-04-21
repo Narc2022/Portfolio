8ec49ea3fdd74410d90eba83222fd9d1b76cb78a
Narc2022 <102596511+Narc2022@users.noreply.github.com>
2022-04-21 08:45:55 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,18 @@ import React from 'react'
 import {BrowserRouter as Router ,Route,Routes} from 'react-router-dom';
 import Home from './components/Home/Home';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
-  return <Router>
+  return (
+  <Router>
     <Header />
     <Routes>
       <Route path="/" element={<Home />}/>
     </Routes>
+    {/* <Footer /> */}
   </Router>
+  );
 }
 
 export default App;
