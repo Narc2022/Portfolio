98b173035aa3be036c39cf39ee3c72b83e90275e
Narc2022 <102596511+Narc2022@users.noreply.github.com>
2022-04-26 17:17:24 +0530

    user update

--- frontend/src/components/Admin/AdminPanel.jsx
+++ frontend/src/components/Admin/AdminPanel.jsx
@@ -1,9 +1,27 @@
+import { Typography } from '@mui/material'
 import React from 'react'
 
 const AdminPanel = () => {
   return (
-    <div>
-        admin
+    <div className="adminPanel">
+        <div className="adminPanelContainer">
+        <Typography variant='h4'>
+                    <p>A</p>
+                    <p>D</p>
+                    <p>M</p>
+                    <p>I</p>
+                    <p style={{marginRight:"1vmax"}}>N</p>
+
+                    <p>P</p>
+                    <p>A</p>
+                    <p>N</p>
+                    <p>E</p>
+                    <p>L</p>
+                </Typography>
+                {/* <form onSubmit={submitHandler}> */}
+
+                {/* </form> */}
+        </div>
     </div>
   )
 }
