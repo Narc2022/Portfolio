d486aeb99c4228d702ca2cbf170542b4aef25d68
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2024-03-17 16:07:53 +0530

    Removed linked in from footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,37 +1,46 @@
-import { Link } from 'react-router-dom';
-import { Typography } from '@mui/material';
-import { BsGithub, BsYoutube, BsInstagram, BsLinkedin } from 'react-icons/bs'
-import React from 'react';
+import { Link } from "react-router-dom";
+import { Typography } from "@mui/material";
+import { BsGithub, BsYoutube, BsInstagram } from "react-icons/bs";
+import React from "react";
 import "./Footer.css";
 const Footer = () => {
-	return (
-	<div className="footer">
-		<div>
-			<Typography variant="h5">About Me</Typography>
-            <Typography>Hey, I'm Sachin Singh, a passionate <b>Software Developer</b> with over 3 years of experience specializing in <b>Front End Development</b>.
-			</Typography>
+  return (
+    <div className="footer">
+      <div>
+        <Typography variant="h5">About Me</Typography>
+        <Typography>
+          Hey, I'm Sachin Singh, a passionate <b>Software Developer</b> with
+          over 3 years of experience specializing in{" "}
+          <b>Front End Development</b>.
+        </Typography>
 
-			<Link to="/contact" className="footerContactBtn">
-			<Typography >Contact Us</Typography>
-			</Link>
-		</div>
-		<div>
-			<Typography variant="h6">Social Media</Typography>
-			<a href='https://github.com/Narc2022' target='blank'>
-				<BsGithub />
-			</a>
-			<a href='https://www.youtube.com/channel/UClXZ6LP5dubwI1We2fVkE7g' target='blank'>
-				<BsYoutube />
-			</a>
-			<a href='https://instagram.com/lets_get_programming_update?igshid=YmMyMTA2M2Y=' target='blank'>
-				<BsInstagram />
-			</a>
-			<a href='https://www.linkedin.com/in/sachin-singh-086aa2167' target='blank'>
+        <Link to="/contact" className="footerContactBtn">
+          <Typography>Contact Us</Typography>
+        </Link>
+      </div>
+      <div>
+        <Typography variant="h6">Social Media</Typography>
+        <a href="https://github.com/Narc2022" target="blank">
+          <BsGithub />
+        </a>
+        <a
+          href="https://www.youtube.com/channel/UClXZ6LP5dubwI1We2fVkE7g"
+          target="blank"
+        >
+          <BsYoutube />
+        </a>
+        <a
+          href="https://instagram.com/lets_get_programming_update?igshid=YmMyMTA2M2Y="
+          target="blank"
+        >
+          <BsInstagram />
+        </a>
+        {/* <a href='https://www.linkedin.com/in/sachins' target='blank'>
 				<BsLinkedin />
-			</a>
-		</div>
-		</div>
-	)
-}
+			</a> */}
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
